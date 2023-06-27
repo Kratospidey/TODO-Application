9cb4ce486028c4dc334316dd122c962d56c6ca5b
kratospidey <paramsinghmakwana@gmail.com>
2023-06-27 05:42:40 +0530

    create basic todo app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  return "Hi"
-}
+	const [newItem, setNewItem] = useState("");
 
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit} className="new-item-form">
+				<div className="form-row"></div>
+				<label htmlFor="item">New Item</label>
+				<input
+					value={newItem}
+					onChange={(e) => {
+						setNewItem(e.target.value);
+					}}
+					type="text"
+					id="item"
+				/>
+				<button className="btn">Add</button>
+			</form>
+			<h1 className="header">Todo List</h1>
+			<ul className="list">
+				<li>
+					<label>
+						<input type="checkbox" />
+						Item 1
+					</label>
+					<button className="btn btn-danger">Delete</button>
+				</li>
+				<li>
+					<label>
+						<input type="checkbox" />
+						Item 2
+					</label>
+					<button className="btn btn-danger">Delete</button>
+				</li>
+			</ul>
+		</>
+	);
+}
